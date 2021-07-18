d57c823cc8e2a7cd369b63bad72d72a008704333
Evandro Lauro <evandrolauro1992@gmail.com>
2021-07-18 19:37:23 -0300

    ...

--- titleCase.js
+++ titleCase.js
@@ -4,18 +4,19 @@ function titleCase(title, minorWords = '') {
     } else {
         let t = title.toLowerCase()
         let m = minorWords.toLowerCase()
-        var listTitle = t.split(" ")
+        let listTitle = t.split(" ")
         let listMinorWords = m.split(" ")
         let list = []
         for (let i = 0; i < listTitle.length; i++) {
             let str = ''
             if (i == 0) {
                 str = listTitle[i][0].toUpperCase() + listTitle[i].substr(1)
-            } else {
+            } 
+            if (i > 0) {
                 str = listMinorWords.find(element => element == listTitle[i])
             }
             if (str == undefined) {
-                str =listTitle[i][0].toUpperCase() + listTitle[i].substr(1)
+                str = listTitle[i][0].toUpperCase() + listTitle[i].substr(1)
             }
             list.push(str)
         }
