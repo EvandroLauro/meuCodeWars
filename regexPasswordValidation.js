0c0f8ab7b4c148ca34e65e882ecdfcf6463340dc
Evandro Lauro <evandrolauro1992@gmail.com>
2021-07-19 16:21:53 -0300

    ...

--- regexPasswordValidation.js
+++ regexPasswordValidation.js
@@ -12,8 +12,6 @@ function validate(password) {
 }
 */
 
-
-
 function validate(password) {
     const re = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/
     return password + ' - Expected ' + re.test(password);
