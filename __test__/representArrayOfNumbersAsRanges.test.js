f67ec3138b995c1e646c40c7c1b93e4c4ae5e52e
Evandro Lauro <evandrolauro1992@gmail.com>
2021-07-23 11:10:17 -0300

    Atualização

--- __test__/representArrayOfNumbersAsRanges.test.js
+++ __test__/representArrayOfNumbersAsRanges.test.js
@@ -3,6 +3,7 @@ const {toRange, toArray} = require('../representArrayOfNumbersAsRanges.js')
 describe("Convert an array to a range", () => {
     test("Test", () => {
         expect(toRange([3,4,5,6,9])).toEqual('3_6,9')
+        expect(toRange([3,4,5,6,9,10,11,12,15])).toEqual('3_6,9_12,15')
     })
 })
 
--- representArrayOfNumbersAsRanges.js
+++ representArrayOfNumbersAsRanges.js
@@ -3,13 +3,38 @@ function toRange(arr) {
     let dentro = []
     let fora = []
     for (let i = 0; i < arr.length; ++i) {
-        arr[0] + i == arr[i] ? dentro.push(arr[i]) : fora.push(arr[i])
+        if (i == 0) {
+            dentro.push(arr[i])
+        } else {
+            if ((arr[i] - arr[i - 1]) == 1) {
+                dentro.push(arr[i])
+            } else {
+                fora.push(arr[i])
+                if (i < (arr.length - 1)) {
+                    dentro.push(arr[i])
+                }
+            }
+        } 
     }
-    return dentro[0] + '_' + dentro[dentro.length - 1] + ',' + fora.toString()
+
+    let result = []
+    for (let i = 0; i < fora.length; ++i) {
+        let index = dentro.indexOf(fora[i])
+        if (dentro[index] != undefined) {
+            result.push(dentro[0] + '_' + dentro[index - 1] + ',' + dentro[index])
+        } else {
+            if (fora.length == 1) {
+                result.push(dentro[0] + '_' +  + dentro[dentro.length - 1] + ',' + fora[i])
+            } else {
+                let r = result
+                result = []
+                result.push(r + '_' + dentro[dentro.length - 1] + ',' + fora[i])
+            }
+        }
+    }
+    return result.toString()
 }
 
-
-// Should return an array
 function toArray(str) {
 }
 
